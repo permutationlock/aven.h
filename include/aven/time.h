4d7abc1e8a4b0488a8bb7834d1f6e38dc299a677
Aven Bross <kieroda@gmail.com>
2024-09-09 15:05:58 -0500

    Renamed time macros to make more sense.

--- include/aven/time.h
+++ include/aven/time.h
@@ -13,9 +13,9 @@
     typedef struct timespec AvenTimeInst;
 #endif
 
-#define AVEN_TIME_NSEC_IN_SEC (1000L * 1000L * 1000L)
-#define AVEN_TIME_USEC_IN_SEC (1000L * 1000L)
-#define AVEN_TIME_MSEC_IN_SEC (1000L)
+#define AVEN_TIME_NSEC_PER_SEC (1000L * 1000L * 1000L)
+#define AVEN_TIME_USEC_PER_SEC (1000L * 1000L)
+#define AVEN_TIME_MSEC_PER_SEC (1000L)
 
 AVEN_FN AvenTimeInst aven_time_now(void);
 AVEN_FN int64_t aven_time_since(AvenTimeInst end, AvenTimeInst start);
@@ -38,12 +38,12 @@ AVEN_FN int64_t aven_time_since(AvenTimeInst end, AvenTimeInst start);
         AvenTimeInst now = {
             .tv_sec = count / freq,
             .tv_nsec = (int)(
-                ((count % freq) * AVEN_TIME_NSEC_IN_SEC + (freq >> 1)) / freq
+                ((count % freq) * AVEN_TIME_NSEC_PER_SEC + (freq >> 1)) / freq
             ),
         };
-        if (now.tv_nsec >= AVEN_TIME_NSEC_IN_SEC) {
+        if (now.tv_nsec >= AVEN_TIME_NSEC_PER_SEC) {
             now.tv_sec += 1;
-            now.tv_nsec -= AVEN_TIME_NSEC_IN_SEC;
+            now.tv_nsec -= AVEN_TIME_NSEC_PER_SEC;
         }
 
         return now;
@@ -63,7 +63,7 @@ AVEN_FN int64_t aven_time_since(AvenTimeInst end, AvenTimeInst start);
 
 AVEN_FN int64_t aven_time_since(AvenTimeInst end, AvenTimeInst start) {
     int64_t seconds = (int64_t)end.tv_sec - (int64_t)start.tv_sec;
-    int64_t sec_diff = seconds * AVEN_TIME_NSEC_IN_SEC;
+    int64_t sec_diff = seconds * AVEN_TIME_NSEC_PER_SEC;
     int64_t nsec_diff = (int64_t)end.tv_nsec - (int64_t)start.tv_nsec;
     return sec_diff + nsec_diff;
 }
